247d5226bfff10c3b58b7f0c4e6e925c93c10778
u002617 <u002617@39edf04f-2723-2346-8cc1-5ef793c3d051>
2010-10-26 14:30:59 +0000

    remove Parameter as Target
    git-svn-id: http://web-svn-srv/repos/ITSWE/ajf/trunk@537 39edf04f-2723-2346-8cc1-5ef793c3d051

--- ajf-core/src/main/java/ajf/persistence/annotations/AutoCommit.java
+++ ajf-core/src/main/java/ajf/persistence/annotations/AutoCommit.java
@@ -7,7 +7,7 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
-@Target({ElementType.FIELD, ElementType.PARAMETER, ElementType.METHOD})
+@Target({ElementType.FIELD, ElementType.METHOD})
 @Inherited
 public @interface AutoCommit {
 
