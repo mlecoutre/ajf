02f78ade2db9438d283885c5ea76e60db269bd03
u002617 <u002617@39edf04f-2723-2346-8cc1-5ef793c3d051>
2010-11-29 15:42:45 +0000

    add utils for beans and classes
    git-svn-id: http://web-svn-srv/repos/ITSWE/ajf/trunk@655 39edf04f-2723-2346-8cc1-5ef793c3d051

--- ajf-core/src/main/java/ajf/utils/BeanUtils.java
+++ ajf-core/src/main/java/ajf/utils/BeanUtils.java
@@ -8,7 +8,7 @@ import java.util.Map;
 
 /**
  * 
- * @author vincent
+ * @author vincent claeysen
  *
  */
 public abstract class BeanUtils {
---
+++ ajf-core/src/main/java/ajf/utils/ClassUtils.java
@@ -0,0 +1,82 @@
+package ajf.utils;
+
+import java.io.File;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class ClassUtils {
+
+	/**
+	 * Utility method used to fetch Class list based on a package name.
+	 * 
+	 * @param packageName
+	 *            (should be the package containing your annotated beans.
+	 */
+	@SuppressWarnings("rawtypes")
+	public static List<Class> getClasses(String packageName) throws Exception {
+		List<Class> classes = new ArrayList<Class>();
+		File directory = null;
+		try {
+			ClassLoader cld = Thread.currentThread().getContextClassLoader();
+			if (cld == null) {
+				throw new ClassNotFoundException("Can't get class loader.");
+			}
+			String path = packageName.replace('.', '/');
+			URL resource = cld.getResource(path);
+			if (resource == null) {
+				throw new ClassNotFoundException("No resource for " + path);
+			}
+			directory = new File(resource.getFile());
+		} catch (NullPointerException x) {
+			throw new ClassNotFoundException(packageName + " (" + directory
+					+ ") does not appear to be a valid package");
+		}
+		if (directory.exists()) {
+			File[] files = directory.listFiles();
+			for (int i = 0; i < files.length; i++) {
+				String fileName = files[i].getName();
+				if (fileName.endsWith(".class")) {
+					// removes the .class extension
+					classes.add(Class.forName(packageName + '.'
+							+ fileName.substring(0, fileName.length() - 6)));
+				}
+			}
+		} else {
+			throw new ClassNotFoundException(packageName
+					+ " is not a valid package");
+		}
+
+		return classes;
+	}
+	
+	/**
+	 * find the entityClass
+	 * @param requestedDAO
+	 * @return the corresponding entity class 
+	 * @throws ClassNotFoundException
+	 */
+	public static Class<?> resolveEntityClass(Class<?> daoClass)
+			throws ClassNotFoundException {
+		String entityClassName = processEntityClassName(daoClass);
+		// retrieve the entity class name
+		Class<?> entityClass = Thread.currentThread()
+				.getContextClassLoader().loadClass(entityClassName);
+		return entityClass;
+	}
+
+	
+	/*
+	 * DAO in [project].core.dao.[Entity]DAO
+	 * Entity in [project].lib.model.[Entity]
+	 */
+	private static String processEntityClassName(Class<?> daoClass) {
+		
+		// replace ".core.dao" by ".lib.model" 
+		String entityClassName = daoClass.getName().replace(".core.dao.", ".lib.model.");
+		// remove ~DAO
+		entityClassName = entityClassName.substring(0, entityClassName.length()-3);
+		return entityClassName;
+	}
+	
+}
