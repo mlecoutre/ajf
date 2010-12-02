b2666e7051882428f3adb9b64b72d48171e28eca
u002617 <u002617@39edf04f-2723-2346-8cc1-5ef793c3d051>
2010-12-02 12:59:20 +0000

    first share
    git-svn-id: http://web-svn-srv/repos/ITSWE/ajf/trunk@659 39edf04f-2723-2346-8cc1-5ef793c3d051

---
+++ ajf-injection/src/main/java/ajf/injection/ServicesTypeListener.java
@@ -0,0 +1,48 @@
+package ajf.injection;
+
+import java.lang.reflect.Field;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.PersistenceUnit;
+
+import com.google.inject.TypeLiteral;
+import com.google.inject.spi.TypeEncounter;
+import com.google.inject.spi.TypeListener;
+
+public class ServicesTypeListener implements TypeListener {
+	
+	private static TypeListener instance = new ServicesTypeListener();
+	
+	private ServicesTypeListener() {
+		super();
+	}
+
+	/**
+	 * 
+	 * @return the implementation
+	 */
+	public static TypeListener getInstance() {
+		return instance;
+	}
+
+	public <T> void hear(TypeLiteral<T> typeLiteral,
+			TypeEncounter<T> typeEncounter) {
+
+		for (Field field : typeLiteral.getRawType().getDeclaredFields()) {
+
+			if (field.getType() == EntityManager.class
+					&& (field.isAnnotationPresent(PersistenceContext.class)
+					|| field.isAnnotationPresent(PersistenceUnit.class))) {
+				typeEncounter.register(new EntityManagerMembersInjector<T>(field));
+			}
+			
+			if (field.isAnnotationPresent(InjectDAO.class)) {
+				typeEncounter.register(new DAOMembersInjector<T>(field));
+			}
+			
+		}
+
+	}
+
+}
