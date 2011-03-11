818adbd770f0011467d5b76a8b0461d2a1f1b1c8
u002617 <u002617@39edf04f-2723-2346-8cc1-5ef793c3d051>
2011-03-11 10:20:27 +0000

    add equals() and hashCode() methods
    git-svn-id: http://web-svn-srv/repos/ITSWE/ajf/trunk@1443 39edf04f-2723-2346-8cc1-5ef793c3d051

--- ajf-core/src/main/java/ajf/cache/impl/InfinispanEmbeddedCacheManagerImpl.java
+++ ajf-core/src/main/java/ajf/cache/impl/InfinispanEmbeddedCacheManagerImpl.java
@@ -44,6 +44,32 @@ public class InfinispanEmbeddedCacheManagerImpl implements CacheManager {
 		this.cacheManager.stop();
 		super.finalize();
 	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		return this.cacheManager.hashCode();
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) return true;
+		if (obj == null) return false;
+		if (getClass() != obj.getClass()) return false;
+		InfinispanEmbeddedCacheManagerImpl other = (InfinispanEmbeddedCacheManagerImpl) obj;
+		if (cacheManager == null) {
+			if (other.cacheManager != null) return false;
+		}
+		else
+			if (!cacheManager.equals(other.cacheManager)) return false;
+		return true;
+	}
+	
 	
 	
 
