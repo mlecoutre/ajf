fab0ae77bb9903f2510376b56fab2048b42e5e1e
u002617 <u002617@39edf04f-2723-2346-8cc1-5ef793c3d051>
2011-10-17 13:10:34 +0000

    test logger
    git-svn-id: http://web-svn-srv/repos/ITSWE/ajf/trunk@2854 39edf04f-2723-2346-8cc1-5ef793c3d051

--- ajf-core/src/test/java/ajf/logger/LoggerFactoryTest.java
+++ ajf-core/src/test/java/ajf/logger/LoggerFactoryTest.java
@@ -1,30 +1,19 @@
 package ajf.logger;
 
-import java.util.List;
-
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 import org.junit.Test;
 import org.slf4j.Logger;
-
-import ch.qos.logback.classic.LoggerContext;
+import org.slf4j.MDC;
 
 public class LoggerFactoryTest {
 
 	@Test
 	public void testLogging() {
 		
-		Log log = LogFactory.getLog("myLogger");
-		log.info("Hello");
-		
 		Logger logger = LoggerFactory.getLogger();
-		logger.info("Just a test");
 		
-		LoggerContext ctx = (LoggerContext) org.slf4j.LoggerFactory.getILoggerFactory();
-		List<ch.qos.logback.classic.Logger> loggersList = ctx.getLoggerList();
-		for (ch.qos.logback.classic.Logger logger2 : loggersList) {
-			System.out.println(logger2 + " " + logger2.getLevel());
-		}
+		MDC.put("userId", "u002617");
+		
+		logger.info("Just a test");
 		
 	}
 	
