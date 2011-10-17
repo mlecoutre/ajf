b00345c3d8f562cb7de3ccb19dee7904a3f4f4c6
u002617 <u002617@39edf04f-2723-2346-8cc1-5ef793c3d051>
2011-10-17 13:19:35 +0000

    refactoring impacts
    git-svn-id: http://web-svn-srv/repos/ITSWE/ajf/trunk@2868 39edf04f-2723-2346-8cc1-5ef793c3d051

--- ajf-injection/src/test/java/ajf/injection/test/BeanUtilsTest.java
+++ ajf-injection/src/test/java/ajf/injection/test/BeanUtilsTest.java
@@ -1,9 +1,10 @@
 package ajf.injection.test;
 
-import static ajf.utils.BeanUtils.newInstance;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
+
+import ajf.utils.BeanUtils;
 
 import com.google.inject.AbstractModule;
 import com.google.inject.Guice;
@@ -15,7 +16,7 @@ public class BeanUtilsTest {
 	
 	@Test
 	public void testInstanciate() throws Exception {
-		MyMock beanMock = (MyMock) newInstance(MyMock.class);
+		MyMock beanMock = (MyMock) BeanUtils.getInstance().newInstance(MyMock.class);
 		assertTrue("check if injection is working",
 				beanMock.getMockDAO() != null);
 	}
