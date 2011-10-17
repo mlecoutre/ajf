448f4f633aafe6a94c960b6af6b75b258f7be896
u002617 <u002617@39edf04f-2723-2346-8cc1-5ef793c3d051>
2011-10-17 13:05:56 +0000

    guice module for configuration
    git-svn-id: http://web-svn-srv/repos/ITSWE/ajf/trunk@2842 39edf04f-2723-2346-8cc1-5ef793c3d051

---
+++ ajf-injection/src/main/java/ajf/injection/ConfigurationInjectionModule.java
@@ -0,0 +1,43 @@
+package ajf.injection;
+
+import java.util.Iterator;
+
+import org.apache.commons.configuration.Configuration;
+import org.slf4j.Logger;
+
+import ajf.logger.LoggerFactory;
+
+import com.google.inject.AbstractModule;
+import com.google.inject.name.Names;
+
+public class ConfigurationInjectionModule extends AbstractModule {
+	
+	private Logger logger = LoggerFactory.getLogger();
+	
+	private Configuration configuration = null;
+	
+	public ConfigurationInjectionModule(Configuration configuration) {
+		super();
+		this.configuration = configuration;
+	}
+
+	@Override
+	protected void configure() {
+		
+		for (Iterator<?> iterator = configuration.getKeys(); iterator.hasNext();) {
+			
+			try {
+				String key = (String) iterator.next();
+				String value = configuration.getString(key);
+				bind(String.class).annotatedWith(Names.named(key)).toInstance(value);
+			} catch (Exception e) {
+				logger.warn("Unattempted exception.", e);
+			}
+			
+		}
+		
+		bind(Configuration.class).toInstance(configuration);
+		
+	}
+
+}
