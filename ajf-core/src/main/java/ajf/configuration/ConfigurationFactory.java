f2623283a7174e688d29033c309322610d27e384
u002617 <u002617@39edf04f-2723-2346-8cc1-5ef793c3d051>
2011-10-17 13:22:31 +0000

    factory for helping in configuration instanciation
    git-svn-id: http://web-svn-srv/repos/ITSWE/ajf/trunk@2873 39edf04f-2723-2346-8cc1-5ef793c3d051

---
+++ ajf-core/src/main/java/ajf/configuration/ConfigurationFactory.java
@@ -0,0 +1,77 @@
+package ajf.configuration;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.apache.commons.configuration.Configuration;
+import org.apache.commons.configuration.ConfigurationException;
+import org.apache.commons.configuration.PropertiesConfiguration;
+
+/**
+ * 
+ * @author vincent
+ * 
+ */
+public abstract class ConfigurationFactory {
+
+	/***
+	 * create Configuration object form resource
+	 * @param resourceName
+	 * @return
+	 * @throws FileNotFoundException
+	 * @throws ConfigurationException
+	 */
+	public static Configuration getConfigurationFromResource(String resourceName)
+			throws FileNotFoundException, ConfigurationException {
+
+		ClassLoader classLoader = Thread.currentThread()
+				.getContextClassLoader();
+		InputStream is = classLoader.getResourceAsStream(resourceName);
+
+		if (null == is)
+			throw new FileNotFoundException("Unable to find resource '"
+					+ resourceName + "'.");
+
+		PropertiesConfiguration pConfig = new PropertiesConfiguration();
+		pConfig.load(is);
+
+		try {
+			is.close();
+		}
+		catch (IOException e) {
+			// Nothing to do
+		}
+		is = null;
+
+		return pConfig;
+
+	}
+
+	/**
+	 * create Configuration object form file
+	 * @param filePath
+	 * @return
+	 * @throws FileNotFoundException
+	 * @throws ConfigurationException
+	 */
+	public static Configuration getConfigurationFromFile(String filePath)
+			throws FileNotFoundException, ConfigurationException {
+
+		File propertieFile = new File(filePath);
+		if (!propertieFile.exists()) {
+			throw new FileNotFoundException("Unable to find the file '"
+					+ filePath + "'.");
+		}
+
+		PropertiesConfiguration pConfig = new PropertiesConfiguration();
+		pConfig.load(propertieFile);
+
+		propertieFile = null;
+
+		return pConfig;
+
+	}
+
+}
