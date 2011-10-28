fcdd0351d7ac96fc61bab4a0f46ab43e2c204c5d
u002617 <u002617@39edf04f-2723-2346-8cc1-5ef793c3d051>
2011-10-28 08:53:48 +0000

    abstract to private constructor
    git-svn-id: http://web-svn-srv/repos/ITSWE/ajf/trunk@2916 39edf04f-2723-2346-8cc1-5ef793c3d051

--- ajf-core/src/main/java/ajf/datas/AuditDataContext.java
+++ ajf-core/src/main/java/ajf/datas/AuditDataContext.java
@@ -1,9 +1,13 @@
 package ajf.datas;
 
-public abstract class AuditDataContext {
-
+public class AuditDataContext {
+	
 	private static ThreadLocal<AuditData> contextData = new ThreadLocal<AuditData>();
 	
+	private AuditDataContext() {
+		super();
+	}
+
 	/**
 	 * 
 	 * @param auditData
@@ -26,6 +30,9 @@ public abstract class AuditDataContext {
 	 * @return
 	 */
 	public static AuditData getAuditData() {
+		AuditData auditData = (AuditData) contextData.get();
+		if (null == auditData)
+			initContextData();
 		return (AuditData) contextData.get();
 	}
 	
