a804562ff334e272554395f61028b5c3e2f18182
u002617 <u002617@39edf04f-2723-2346-8cc1-5ef793c3d051>
2011-11-08 14:46:13 +0000

    from ajf-core
    git-svn-id: http://web-svn-srv/repos/ITSWE/ajf/trunk@2977 39edf04f-2723-2346-8cc1-5ef793c3d051

---
+++ ajf-commons/src/main/java/ajf/datas/EditableAuditData.java
@@ -0,0 +1,19 @@
+package ajf.datas;
+
+
+public interface EditableAuditData extends AuditData {
+
+	/**
+	 * Put a value for an audit data.
+	 * Predefine key are available in using AuditData.KEY_XXXX
+	 * @param key audit data key
+	 * @param value value to push in the audit data.
+	 */
+	public void put(String key, Object value);
+	
+	/**
+	 * clean the AuditData element
+	 */
+	public void clear();
+
+}
