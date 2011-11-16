26cac0efc94baea7bec6ec00c2fcd91cd37eb9dc
u002617 <u002617@39edf04f-2723-2346-8cc1-5ef793c3d051>
2011-11-16 16:15:52 +0000

    old EventContext
    git-svn-id: http://web-svn-srv/repos/ITSWE/ajf/trunk@3052 39edf04f-2723-2346-8cc1-5ef793c3d051

---
+++ ajf-monitoring/src/test/java/ajf/monitoring/EventManagerTest.java
@@ -0,0 +1,98 @@
+package ajf.monitoring;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.nio.charset.Charset;
+
+import javax.xml.bind.JAXBException;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.slf4j.Logger;
+
+import ajf.logger.LoggerFactory;
+import ajf.monitoring.impl.ConsoleEmitter;
+import ajf.monitoring.impl.XmlJAXBFormatter;
+
+public class EventManagerTest {
+
+	@SuppressWarnings("unused")
+	private static final Logger logger = LoggerFactory.getLogger();
+
+	@Test
+	public void testSendSimpleEvent() throws JAXBException, IOException,
+			InterruptedException {
+
+		EventManager manager = new EventManager();
+
+		ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+		PrintStream printStream = new PrintStream(outputStream, true);
+		EventEmitter emitter = new ConsoleEmitter(printStream);
+
+		EventHandler handler = new EventHandler(new XmlJAXBFormatter(
+				MyEvent.class), emitter);
+		manager.installDefaultEventHandler(handler);
+
+		MyEvent event = new MyEvent("Albert", "Dupont");
+		manager.sendEvent(event);
+
+		manager.close();
+
+		// wait some time
+		Thread.sleep(100);
+
+		printStream.flush();
+
+		String utf8 = Charset.forName("UTF-8").displayName();
+		String res = outputStream.toString(utf8);
+
+		printStream.close();
+		outputStream.close();
+
+		Assert.assertTrue("The message has not been published.", (null != res)
+				&& (0 < res.length()));
+
+	}
+
+	@Test
+	public void testSendRegisteredEvent() throws JAXBException, IOException, InterruptedException {
+
+		EventManager manager = new EventManager();
+
+		ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+		PrintStream printStream = new PrintStream(outputStream, true);
+		EventEmitter emitter = new ConsoleEmitter(printStream);
+
+		EventHandler handler = new EventHandler(new XmlJAXBFormatter(
+				MyEvent.class), emitter);
+		manager.installDefaultEventHandler(handler);
+
+		String eventType = "event/test";
+		manager.registerEvent(eventType, MyEvent.class);
+
+		MyEvent event = manager.newEvent(eventType);
+		event.setFirstName("Bob");
+		event.setLastName("Durand");
+
+		manager.sendEvent(event);
+
+		manager.close();
+
+		// wait some time
+		Thread.sleep(100);
+
+		printStream.flush();
+
+		String utf8 = Charset.forName("UTF-8").displayName();
+		String res = outputStream.toString(utf8);
+
+		printStream.close();
+		outputStream.close();
+
+		Assert.assertTrue("The message has not been published.", (null != res)
+				&& (0 < res.length()));
+
+	}
+
+}
