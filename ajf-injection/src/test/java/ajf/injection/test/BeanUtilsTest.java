01fde7be5a2b118c82d5550974e7466ff5265021
u002617 <u002617@39edf04f-2723-2346-8cc1-5ef793c3d051>
2011-11-16 16:20:24 +0000

    git-svn-id: http://web-svn-srv/repos/ITSWE/ajf/trunk@3060 39edf04f-2723-2346-8cc1-5ef793c3d051

--- ajf-injection/src/test/java/ajf/injection/test/BeanUtilsTest.java
+++ ajf-injection/src/test/java/ajf/injection/test/BeanUtilsTest.java
@@ -16,7 +16,7 @@ public class BeanUtilsTest {
 	
 	@Test
 	public void testInstanciate() throws Exception {
-		MyMock beanMock = (MyMock) BeanUtils.getInstance().newInstance(MyMock.class);
+		MyMock beanMock = (MyMock) BeanUtils.newInstance(MyMock.class);
 		assertTrue("check if injection is working",
 				beanMock.getMockDAO() != null);
 	}
