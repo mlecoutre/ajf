6abb316f87e5b4f7ef149ed4870c7c3ef19afb6e
u002617 <u002617@39edf04f-2723-2346-8cc1-5ef793c3d051>
2011-11-16 16:11:59 +0000

    set methods as static
    git-svn-id: http://web-svn-srv/repos/ITSWE/ajf/trunk@3045 39edf04f-2723-2346-8cc1-5ef793c3d051

--- ajf-commons/src/main/java/ajf/utils/BeanUtils.java
+++ ajf-commons/src/main/java/ajf/utils/BeanUtils.java
@@ -15,24 +15,25 @@ public class BeanUtils {
 		
 	private static final Logger logger = LoggerFactory.getLogger(BeanUtils.class);
 	
-	private static final BeanUtils instance = new BeanUtils(); 
+	private static boolean initialized = false;
+	private static final Object token = new Object();
 	
-	private boolean initialized = false;
-	private Object token = new Object();
+	private static BeanUtilsDelegate beanUtilsDelegate = null;
 	
-	private BeanUtilsDelegate beanUtilsDelegate = null;
+	private static BeanUtils instance = new BeanUtils();
 		
 	private BeanUtils() {
 		super();
 	}
 	
+	@Deprecated
 	public static BeanUtils getInstance() {
 		return instance;
 	}
-	
-	public void setBeanUtilsDelegate(BeanUtilsDelegate beanUtilsDelegate) {
-		logger.info("set BeanUtilsDelegate to instance of: ".concat(beanUtilsDelegate.getClass().getName()));
-		this.beanUtilsDelegate = beanUtilsDelegate;
+
+	public static void setBeanUtilsDelegate(BeanUtilsDelegate delegate) {
+		logger.info("set BeanUtilsDelegate to instance of: ".concat(delegate.getClass().getName()));
+		beanUtilsDelegate = delegate;
 	}
 
 	/**
@@ -43,7 +44,7 @@ public class BeanUtils {
 	 * @throws IllegalAccessException
 	 */
 	@SuppressWarnings("unchecked")
-	public <T> T newInstance(Class<?> clazz)
+	public static <T> T newInstance(Class<?> clazz)
 			throws RuntimeException {
 		
 		checkInit();
@@ -79,24 +80,37 @@ public class BeanUtils {
 	
 	/**
 	 * 
+	 * @param beanInstance
 	 */
-	private void checkInit() {
+	public static void initialize(Object beanInstance) {
+		
+		checkInit();
+		
+		if (null != beanUtilsDelegate)
+			beanUtilsDelegate.initialize(beanInstance);
+	}
+	
+	/**
+	 * 
+	 */
+	private static void checkInit() {
 		
 		if (!initialized) {
 			synchronized (token) { 
 				if (!initialized) {
-					initialized = true;
 					try {
 						ServiceLoader<BeanUtilsDelegate> budLoader = ServiceLoader.load(BeanUtilsDelegate.class);
 						if (null != budLoader) {
 							for (BeanUtilsDelegate bud : budLoader) {
 								logger.info("Use BeanUtilsDelegate instance of: ".concat(bud.getClass().getName()));
-								this.beanUtilsDelegate = bud;
+								beanUtilsDelegate = bud;
+								break;
 							}
 						}
 					} catch (Exception e) {
 						logger.warn("Unable to get resources 'META-INF/services/ajf.utils.BeanUtilsDelegate'.", e);
 					}
+					initialized = true;
 				}
 			}
 		}
