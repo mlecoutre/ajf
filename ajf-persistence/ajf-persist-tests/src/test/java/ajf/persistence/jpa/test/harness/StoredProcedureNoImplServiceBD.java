24e7382f35121b40e17388f7f863ea58ffcdba22
u002617 <u002617@39edf04f-2723-2346-8cc1-5ef793c3d051>
2012-01-12 14:42:45 +0000

    qualifying parameters
    git-svn-id: http://web-svn-srv/repos/ITSWE/ajf/trunk@3207 39edf04f-2723-2346-8cc1-5ef793c3d051

--- ajf-persistence/ajf-persist-tests/src/test/java/ajf/persistence/jpa/test/harness/StoredProcedureNoImplServiceBD.java
+++ ajf-persistence/ajf-persist-tests/src/test/java/ajf/persistence/jpa/test/harness/StoredProcedureNoImplServiceBD.java
@@ -3,16 +3,18 @@ package ajf.persistence.jpa.test.harness;
 import java.util.List;
 
 import ajf.persistence.jpa.annotation.DataSource;
+import ajf.persistence.jpa.annotation.In;
 import ajf.persistence.jpa.annotation.StoredProcedure;
 
 @DataSource("jdbc/myDatasource")
 public interface StoredProcedureNoImplServiceBD {
 
-	@StoredProcedure(name="ZZTESTNOPARAM", resultClass=ModelSp.class)
+	@StoredProcedure(name="ZZTESTNOPARAM)", resultClass=ModelSp.class)
 	List<ModelSp> findAllModels();
 	
-	@StoredProcedure(name="ZZTESTWITHPARAM", resultClass=ModelSp.class)
-	List<ModelSp> findAllModelsByName(String name);
+	@StoredProcedure(name="ZZTESTWITHPARAM(:name)", resultClass=ModelSp.class)
+	List<ModelSp> findAllModelsByName(
+			@In("name")	String name);
 	
 	@StoredProcedure(name="ZZTESTONEWITHPARAM", resultClass=ModelSp.class)
 	ModelSp findOneModelByName(String name);
