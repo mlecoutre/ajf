6036dfc51659d5b5444b54e134275939c4886d35
u002617 <u002617@39edf04f-2723-2346-8cc1-5ef793c3d051>
2012-01-12 14:36:50 +0000

    add DataSource qualifier
    git-svn-id: http://web-svn-srv/repos/ITSWE/ajf/trunk@3206 39edf04f-2723-2346-8cc1-5ef793c3d051

--- ajf-persistence/ajf-persist-tests/src/test/java/ajf/persistence/jpa/test/harness/StoredProcedureNoImplServiceBD.java
+++ ajf-persistence/ajf-persist-tests/src/test/java/ajf/persistence/jpa/test/harness/StoredProcedureNoImplServiceBD.java
@@ -2,8 +2,10 @@ package ajf.persistence.jpa.test.harness;
 
 import java.util.List;
 
+import ajf.persistence.jpa.annotation.DataSource;
 import ajf.persistence.jpa.annotation.StoredProcedure;
 
+@DataSource("jdbc/myDatasource")
 public interface StoredProcedureNoImplServiceBD {
 
 	@StoredProcedure(name="ZZTESTNOPARAM", resultClass=ModelSp.class)
