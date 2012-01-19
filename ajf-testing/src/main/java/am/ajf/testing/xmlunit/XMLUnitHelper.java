d8b7303be146b5a97f37b7f8c03e3b8b4665ff34
u002617 <u002617@39edf04f-2723-2346-8cc1-5ef793c3d051>
2012-01-19 10:47:01 +0000

    refactored as am.ajf
    git-svn-id: http://web-svn-srv/repos/ITSWE/ajf/trunk@3249 39edf04f-2723-2346-8cc1-5ef793c3d051

---
+++ ajf-testing/src/main/java/am/ajf/testing/xmlunit/XMLUnitHelper.java
@@ -0,0 +1,46 @@
+package am.ajf.testing.xmlunit;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.nio.charset.Charset;
+
+import org.custommonkey.xmlunit.XMLTestCase;
+import org.custommonkey.xmlunit.XMLUnit;
+import org.xml.sax.EntityResolver;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+
+public class XMLUnitHelper {
+	
+	private XMLUnitHelper() {
+		super();
+	}
+
+	public static XMLTestCase newTestCase() {
+		
+		XMLTestCase xmlTestCase = new XMLTestCase() {};
+		return xmlTestCase;
+		
+	}
+
+	public static void initialize() {
+		
+		XMLUnit.setIgnoreAttributeOrder(true);
+		XMLUnit.setIgnoreComments(true);
+		XMLUnit.setIgnoreDiffBetweenTextAndCDATA(true);
+		XMLUnit.setIgnoreWhitespace(true);
+		XMLUnit.setTestEntityResolver(new EntityResolver() {
+			
+			@Override
+			public InputSource resolveEntity(String publicId, String systemId)
+					throws SAXException, IOException {
+				String empty = "";
+				ByteArrayInputStream bais =
+					new ByteArrayInputStream(empty.getBytes(Charset.forName("UTF-8")));
+				return new InputSource(bais);
+			}
+		});
+		
+	}
+
+}
---
+++ ajf-testing/src/main/resources/readme.txt
@@ -0,0 +1,2 @@
+Ref : 
+http://en.wikipedia.org/wiki/Unit_testing#Unit_testing_frameworks
