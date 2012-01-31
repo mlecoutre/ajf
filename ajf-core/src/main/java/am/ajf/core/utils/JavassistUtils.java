bd6f38abdfcc8a6ffbfd014a03caf21c78f5b67e
e016696 <e016696@39edf04f-2723-2346-8cc1-5ef793c3d051>
2012-01-31 08:37:20 +0000

    [ADD] Javaassist helper
    git-svn-id: http://web-svn-srv/repos/ITSWE/ajf/trunk@3332 39edf04f-2723-2346-8cc1-5ef793c3d051

---
+++ ajf-core/src/main/java/am/ajf/core/utils/JavassistUtils.java
@@ -0,0 +1,61 @@
+package am.ajf.core.utils;
+
+import javassist.CannotCompileException;
+import javassist.ClassPool;
+import javassist.CtClass;
+import javassist.CtField;
+import javassist.NotFoundException;
+
+/**
+ * This helper class contain commonly used methods to manipulate
+ * Javaassist component that doesnt exist in the existing Javaassist helper.
+ * 
+ * 
+ * @author Nicolas Radde (E016696)
+ *
+ */
+public class JavassistUtils {
+	
+	/**
+	 * Generate a SLF4J Logger field that can be used in your Javassist class. 
+	 * 
+	 * @param cc the class that will contain the field
+	 * @return the newly created logger instance
+	 * @throws CannotCompileException
+	 */
+	public static CtField createLogger(CtClass cc) throws CannotCompileException {
+		CtField cLogger = CtField.make("private final transient org.slf4j.Logger logger = org.slf4j.LoggerFactory.getLogger(this.getClass());", cc);				
+		return cLogger;
+	}
+
+	/**
+	 * Create a new Class, that extends superClass if it's not null and that
+	 * implement the given interface 
+	 * 
+	 * @param superClass can be null
+	 * @param interfaceClass can't be null
+	 * @param pool 
+	 * @return the newly created class
+	 * @throws NotFoundException
+	 * @throws CannotCompileException
+	 */
+	public static CtClass initClass(Class<?> superClass, Class<?> interfaceClass, ClassPool pool)
+			throws NotFoundException, CannotCompileException {
+				CtClass cc;
+				CtClass cin = pool.get(interfaceClass.getName());
+				if (superClass == null) {// no impl, so impl the interface
+					cc = pool.makeClass(interfaceClass.getName() + generateClassSuffix());		
+					cc.setInterfaces(new CtClass[] {cin});
+				} else { //extend the provided client impl
+					cc = pool.makeClass(superClass.getName() + generateClassSuffix());		
+					CtClass cim = pool.get(superClass.getName());
+					cc.setSuperclass(cim);
+				}
+				return cc;
+			}
+
+	private static String generateClassSuffix() {
+		return "_$ajf$javaassist$proxy$";
+	}
+
+}
