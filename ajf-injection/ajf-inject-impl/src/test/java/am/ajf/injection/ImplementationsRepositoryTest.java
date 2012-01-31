d25ce67cbe2085ee732ad6bda040918de51afed9
u002617 <u002617@39edf04f-2723-2346-8cc1-5ef793c3d051>
2012-01-31 10:49:45 +0000

    remove unrequired imports
    git-svn-id: http://web-svn-srv/repos/ITSWE/ajf/trunk@3337 39edf04f-2723-2346-8cc1-5ef793c3d051

--- ajf-injection/ajf-inject-impl/src/test/java/am/ajf/injection/ImplementationsRepositoryTest.java
+++ ajf-injection/ajf-inject-impl/src/test/java/am/ajf/injection/ImplementationsRepositoryTest.java
@@ -5,8 +5,6 @@ import org.junit.Before;
 import org.junit.Test;
 import org.testng.Assert;
 
-import am.ajf.core.ApplicationContext;
-
 /**
  * Test the repository management and especially the addService method.
  * 
