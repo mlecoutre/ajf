72108e9b32e94cce0f855cbb4c699be1d73ae2e0
u002617 <u002617@39edf04f-2723-2346-8cc1-5ef793c3d051>
2012-02-01 13:39:20 +0000

    MonitoringServlet Wrapper
    git-svn-id: http://web-svn-srv/repos/ITSWE/ajf/trunk@3358 39edf04f-2723-2346-8cc1-5ef793c3d051

---
+++ ajf-web/src/main/java/am/ajf/web/MonitoringServlet.java
@@ -0,0 +1,162 @@
+package am.ajf.web;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+
+import am.ajf.core.logger.LoggerFactory;
+
+/**
+ * Default Monitoring Servlet Delegate
+ * 
+ * @author u002617
+ */
+public class MonitoringServlet extends HttpServlet {
+
+	private static final Logger logger = LoggerFactory.getLogger(MonitoringServlet.class);
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	private static final String MONITORING_SERVLET_CLASS_PARAMETER = "MonitoringServletClass";
+
+	private static final String DEFAULT_MONITORING_SERVLET = "ajf.monitoring.web.WASMonitoringServlet";
+	private static HttpServlet monitoringServlet = null;
+
+	private static final String STATUS_OK = "OK";
+	private static final long MBYTES = 1024 * 1024;
+
+	/**
+	 * The response content type: text/html
+	 */
+	private static final String CONTENT_TYPE = "text/html";
+
+	/**
+	 * 
+	 */
+	public MonitoringServlet() {
+		super();
+	}
+
+	/**
+	 * Return "OK".
+	 * 
+	 * @param request
+	 *            a <code>HttpServletRequest</code> value
+	 * @param response
+	 *            a <code>HttpServletResponse</code> value
+	 * @throws ServletException
+	 *             if an error occurs
+	 * @throws IOException
+	 *             if an error occurs
+	 */
+	protected void service(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+
+		if (null != monitoringServlet) {
+			try {
+				monitoringServlet.service(request, response);
+				return;
+			}
+			catch (Exception e) {
+				logger.error("Monitoring delegate failure", e);
+			}
+		}
+
+		PrintWriter out = response.getWriter();
+		response.setContentType(CONTENT_TYPE);
+
+		/* Partie Declarative */
+		String parameter = request.getQueryString();
+		String result = STATUS_OK;
+
+		/* Partie Executive */
+		// Test le mode de la servlet (Surveillance ou Informations)
+		if (parameter != null) {
+			if ((parameter.indexOf("monitor") < 0)
+					|| (parameter.indexOf("news") >= 0)) {
+				// Mode Information
+				result = doModeInformation();
+			}
+		}
+		out.write(result);
+		out.flush();
+
+	}
+
+	/*
+	 * Description: retourne la page HTML a afficher
+	 */
+	protected String doModeInformation() {
+
+		/* Partie Declarative */
+		StringBuffer result = new StringBuffer(
+				"<HTML><HEAD><TITLE>Monitoring Servlet</TITLE></HEAD><BODY><CENTER><P><H1>Monitoring Servlet</H1></P><BR><P><TABLE>");
+		result.append("<TR><TD bgcolor='#00FF00'><P align ='center'><B>&nbspJava Virtual Machine&nbsp</B><BR><SMALL>");
+
+		result.append("Total Memory: ")
+				.append(Runtime.getRuntime().totalMemory() / MBYTES)
+				.append(" Mo<BR>");
+		result.append("Free Memory: ")
+				.append(Runtime.getRuntime().freeMemory() / MBYTES)
+				.append(" Mo<BR>");
+		result.append("Max Memory: ")
+				.append(Runtime.getRuntime().maxMemory() / MBYTES)
+				.append(" Mo<BR>");
+
+		result.append("</SMALL></P></TD></TR>");
+		result.append("</TABLE></P><P><B>Contact: </B>");
+		result.append(getInitParameter("contact"));
+		result.append("</P></CENTER></BODY></HTML>");
+
+		return result.toString();
+
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see javax.servlet.Servlet#init(javax.servlet.ServletConfig)
+	 */
+	public void init(ServletConfig config) throws ServletException {
+
+		String className = DEFAULT_MONITORING_SERVLET;
+
+		String param = config
+				.getInitParameter(MONITORING_SERVLET_CLASS_PARAMETER);
+		if ((null == param) || (param.trim().isEmpty())) {
+			try {
+				Class.forName(param);
+				className = param;
+			}
+			catch (ClassNotFoundException e) {
+				logger.error(
+						"Unable to find class ".concat(className).concat(
+								" for Monitoring Delegate"), e);
+			}
+		}
+
+		try {
+			monitoringServlet = (HttpServlet) Class.forName(className)
+					.newInstance();
+			monitoringServlet.init(config);
+			logger.info("Delegate Monitoring to ".concat(className));
+		}
+		catch (Exception e) {
+			logger.error(
+					"Delegate Monitoring to ".concat(className).concat(
+							" failure"), e);
+		}
+
+	}
+
+}
