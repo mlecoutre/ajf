fa46108fead8c7448529adf30d67041571020fab
u002617 <u002617@39edf04f-2723-2346-8cc1-5ef793c3d051>
2012-02-02 08:28:30 +0000

    git-svn-id: http://web-svn-srv/repos/ITSWE/ajf/trunk@3379 39edf04f-2723-2346-8cc1-5ef793c3d051

--- ajf-showcase-simple/src/main/java/am/ajf/showcase/web/controllers/PersonMBean.java
+++ ajf-showcase-simple/src/main/java/am/ajf/showcase/web/controllers/PersonMBean.java
@@ -6,10 +6,15 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.Set;
 
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
@@ -19,8 +24,8 @@ import ajf.injection.InjectionContext;
 import ajf.logger.injection.InjectLogger;
 import ajf.services.injection.InjectService;
 import am.ajf.showcase.lib.business.EmployeeManagementBD;
-import am.ajf.showcase.lib.dto.ListEmployeesPB;
-import am.ajf.showcase.lib.dto.ListEmployeesRB;
+import am.ajf.showcase.lib.business.dto.ListEmployeesPB;
+import am.ajf.showcase.lib.business.dto.ListEmployeesRB;
 import am.ajf.showcase.lib.model.Person;
 
 @ManagedBean
@@ -28,18 +33,28 @@ import am.ajf.showcase.lib.model.Person;
 public class PersonMBean implements Serializable {
 
 	private static final long serialVersionUID = 1L;
+	
+	public interface EditEmployeeCheck {
+		
+	}
+	
+	/*
+	 * will be replaced by @Inject in AJF 2.0.2
+	 */
+	@InjectLogger
+	private Logger logger;
 
 	/* ATTRIBUTES */
 	private String firstname;
 
-	@Size(min = 1, max = 32)
+	@Size(groups= EditEmployeeCheck.class, min = 2, max = 32)
 	@NotNull
 	private String lastname;
 
 	private String sex;
 	private Date birthday;
 	private List<Person> employees = new ArrayList<Person>();
-
+	
 	/* POLICIES BUSINESS DELEGATE */
 	/*
 	 * will be replaced by @Inject in AJF 2.0.2
@@ -50,14 +65,11 @@ public class PersonMBean implements Serializable {
 	/* CONSTANTS */
 	//private int MAX_RESULT = 200;
 
-	/*
-	 * will be replaced by @Inject in AJF 2.0.2
+	/**
+	 * DEFAULT Constructor
 	 */
-	@InjectLogger
-	private Logger logger;
-
 	public PersonMBean() {
-
+		super();
 	}
 
 	@PostConstruct
@@ -68,29 +80,53 @@ public class PersonMBean implements Serializable {
 		InjectionContext.getInstance().getInjector().injectMembers(this);
 	}
 
-	/**
-	 * DEFAULT Constructor
-	 */
 	public void createEmployee() {
 
 	}
 
 	public void listEmployees() {
+		
+		/*
+		DefaultAuditData auditData = new DefaultAuditData();
+		auditData.put(AuditData.KEY_USERID, "User");
+		AuditDataContext.initContextData(auditData);
+		*/
+		
 		logger.debug("listEmployees MBean");
 		try {
-			// Initialize parambean
-			ListEmployeesPB pb = new ListEmployeesPB(lastname);
+			
+			Validator validator = getValidator();
+			Set<ConstraintViolation<PersonMBean>> constraintViolations = validator.validate(this, EditEmployeeCheck.class);
+			
+			if (constraintViolations.isEmpty()) {
+				// Initialize parambean
+				ListEmployeesPB pb = new ListEmployeesPB(lastname);
 
-			// Call business layer
-			ListEmployeesRB rb = employeeManagement.listEmployees(pb);
+				// Call business layer
+				ListEmployeesRB rb = employeeManagement.listEmployees(pb);
 
-			// Get Result
-			employees = rb.getEmployees();
-
+				// Get Result
+				employees = rb.getEmployees();
+			}
+			else {
+				/*
+				FacesMessage facesMessage = new FacesMessage(
+						FacesMessage.SEVERITY_ERROR,
+						WebUtils.getFieldLabel("application.error.occured"), message);
+				FacesContext.getCurrentInstance().addMessage(clientId, facesMessage);
+				*/
+			}
+			
 		} catch (Exception e) {
 			// import static ajf.web.WebUtils.*;
 			handleError(e, logger, "Error when try to list employees");
 		}
+	}
+
+	private Validator getValidator() {
+		ValidatorFactory validatorFactory = Validation.buildDefaultValidatorFactory();
+		Validator validator = validatorFactory.getValidator();
+		return validator;
 	}
 
 	public String getSex() {
