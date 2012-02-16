88e95a42af7911ec84b8d4a9f47e1c2f05aa390a
u002617 <u002617@39edf04f-2723-2346-8cc1-5ef793c3d051>
2012-02-16 14:53:23 +0000

    allow to manage EventsDomains
    git-svn-id: http://web-svn-srv/repos/ITSWE/ajf/trunk@3497 39edf04f-2723-2346-8cc1-5ef793c3d051

---
+++ ajf-monitoring/src/main/java/am/ajf/monitoring/EventsDomainManager.java
@@ -0,0 +1,82 @@
+package am.ajf.monitoring;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+import org.slf4j.Logger;
+
+import am.ajf.core.logger.LoggerFactory;
+
+public class EventsDomainManager {
+
+	@SuppressWarnings("unused")
+	private final static Logger logger = LoggerFactory
+			.getLogger(EventsDomainManager.class);
+
+	private static EventsDomain defaultEventsDomain = null;
+	private static Map<String, EventsDomain> eventsDomainsMap = new ConcurrentHashMap<String, EventsDomain>();
+
+	static {
+		initClass();
+	}
+
+	/**
+	 * init impls map
+	 */
+	private static void initClass() {
+
+		defaultEventsDomain = new EventsDomain();
+
+	}
+
+	private EventsDomainManager() {
+		super();
+	}
+
+	/**
+	 * return the default EventsDomain
+	 * 
+	 * @return default EventsDomain instance
+	 * @throws NullPointerException
+	 */
+	public static EventsDomain getEventsDomain() throws NullPointerException {
+
+		return defaultEventsDomain;
+	}
+
+	/**
+	 * return a nammed EventsDomain instance
+	 * 
+	 * @param eventsDomainName
+	 * @return an EventsDomain instance
+	 * @throws NullPointerException
+	 */
+	public static EventsDomain getEventsDomain(String eventsDomainName)
+			throws NullPointerException {
+
+		EventsDomain eventsDomain = null;
+		if (!eventsDomainsMap.containsKey(eventsDomainName)) {
+			createEventsDomain(eventsDomainName);
+		}
+
+		eventsDomain = eventsDomainsMap.get(eventsDomainName);
+		return eventsDomain;
+	}
+
+	protected static synchronized void createEventsDomain(
+			String eventManagerName) {
+		EventsDomain eventManager = new EventsDomain();
+		eventsDomainsMap.put(eventManagerName, eventManager);
+	}
+
+	/**
+	 * return the EventManager names
+	 * 
+	 * @return
+	 */
+	public static Collection<String> getEventsDomainNames() {
+		return eventsDomainsMap.keySet();
+	}
+
+}
