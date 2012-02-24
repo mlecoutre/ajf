df8e996755fb94b3f63a0885b073cc5661ab4e87
e016696 <e016696@39edf04f-2723-2346-8cc1-5ef793c3d051>
2012-02-24 17:21:24 +0000

    ASSIGNED - bug 19267: Merge the ajf-persistence sub projects into one http://bugtracking.arcelor.net/show_bug.cgi?id=19267
    [FIX] failed delete commit
    
    git-svn-id: http://web-svn-srv/repos/ITSWE/ajf/trunk@3574 39edf04f-2723-2346-8cc1-5ef793c3d051

--- ajf-persistence/ajf-persist-tests/src/test/java/am/ajf/persistence/jpa/test/harness/SimpleCrudServiceBD.java
+++
@@ -1,7 +0,0 @@
-package am.ajf.persistence.jpa.test.harness;
-
-import am.ajf.persistence.jpa.api.CrudServiceBD;
-
-public interface SimpleCrudServiceBD extends CrudServiceBD<ModelCrud, Long> {	
-	//.... Your Additional Methods ...
-}
