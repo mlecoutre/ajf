89c1c9c80a8d48179331e8e97d31918ee7a6543b
u002617 <u002617@39edf04f-2723-2346-8cc1-5ef793c3d051>
2012-03-06 16:00:18 +0000

    update documents
    git-svn-id: http://web-svn-srv/repos/ITSWE/ajf/trunk@3630 39edf04f-2723-2346-8cc1-5ef793c3d051

---
+++ ajf-documentation/src/main/docbook/ajf-core.mm
@@ -0,0 +1,192 @@
+<map version="0.9.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node ID="ID_1434008853" CREATED="1329474020309" MODIFIED="1329476071908" STYLE="bubble">
+<richcontent TYPE="NODE">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b>ajf-core</b>&#160;documentation content
+    </p>
+  </body>
+</html>
+</richcontent>
+<hook NAME="MapStyle" layout="OUTLINE" max_node_width="600"/>
+<node TEXT="ApplicationContext" POSITION="right" ID="ID_1794059534" CREATED="1329475922667" MODIFIED="1329476733347">
+<node ID="ID_705828264" CREATED="1329476570053" MODIFIED="1329477250432">
+<richcontent TYPE="NODE">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      - manage &quot;<b>LogDir</b>&quot; and &quot;<b>WorkingDir</b>&quot; variables
+    </p>
+    <p>
+      - attach the &quot;<b>settings.properties</b>&quot; <b>configuration</b>&#160;file
+    </p>
+    <p>
+      - usage
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node TEXT="Configuration" POSITION="right" ID="ID_1134765217" CREATED="1329476737599" MODIFIED="1329476744700">
+<node ID="ID_47841870" CREATED="1329476755041" MODIFIED="1329477255917">
+<richcontent TYPE="NODE">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      - main operations/features (placeholders, evaluate)
+    </p>
+    <p>
+      - configuration sources
+    </p>
+    <p>
+      - configurations merge
+    </p>
+    <p>
+      - configuration listener
+    </p>
+    <p>
+      - usage
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node TEXT="AuditDatas" POSITION="left" ID="ID_1470874266" CREATED="1329475962721" MODIFIED="1329475971107">
+<node ID="ID_744167936" CREATED="1329477055429" MODIFIED="1329477263692">
+<richcontent TYPE="NODE">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      - role
+    </p>
+    <p>
+      - content
+    </p>
+    <p>
+      - edition
+    </p>
+    <p>
+      - usage
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node TEXT="Mail" POSITION="right" ID="ID_911293866" CREATED="1329476006107" MODIFIED="1329476008471">
+<node ID="ID_814976549" CREATED="1329477007314" MODIFIED="1329477270750">
+<richcontent TYPE="NODE">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      - dataModel
+    </p>
+    <p>
+      - configuration
+    </p>
+    <p>
+      - message templating
+    </p>
+    <p>
+      - usage
+    </p>
+  </body>
+</html>
+</richcontent>
+<icon BUILTIN="idea"/>
+</node>
+</node>
+<node TEXT="Cache" POSITION="left" ID="ID_952111654" CREATED="1329476013679" MODIFIED="1329476016183">
+<node ID="ID_617978615" CREATED="1329477200696" MODIFIED="1329477280894">
+<richcontent TYPE="NODE">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      - role
+    </p>
+    <p>
+      - dataModel
+    </p>
+    <p>
+      - implementations
+    </p>
+    <p>
+      - usage
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node TEXT="Exceptions" POSITION="right" ID="ID_711823335" CREATED="1329476022302" MODIFIED="1329476026568">
+<node ID="ID_335322622" CREATED="1329477287983" MODIFIED="1329477333969">
+<richcontent TYPE="NODE">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      - typed exceptions
+    </p>
+    <p>
+      - usage
+    </p>
+    <p>
+      - exception handling
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node TEXT="Utils" POSITION="left" ID="ID_1247406073" CREATED="1329476056887" MODIFIED="1329477351580">
+<icon BUILTIN="idea"/>
+<node ID="ID_649524816" CREATED="1329477357190" MODIFIED="1329477513735">
+<richcontent TYPE="NODE">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      - BeanUtils main methods
+    </p>
+    <p>
+      - ClassUtils main methods
+    </p>
+    <p>
+      - StringUtils main methods
+    </p>
+    <p>
+      - URLEncoder main methods
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</node>
+</map>
