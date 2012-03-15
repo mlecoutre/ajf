5bb4d680156560f2beadcc5ac5a1b9c0b2ebea58
u002617 <u002617@39edf04f-2723-2346-8cc1-5ef793c3d051>
2012-03-15 13:36:06 +0000

    work with BeansManager
    git-svn-id: http://web-svn-srv/repos/ITSWE/ajf/trunk@3708 39edf04f-2723-2346-8cc1-5ef793c3d051

--- ajf-injection/src/main/java/am/ajf/injection/MailSenderProducer.java
+++ ajf-injection/src/main/java/am/ajf/injection/MailSenderProducer.java
@@ -1,29 +1,27 @@
 package am.ajf.injection;
 
+//import javax.enterprise.inject.Produces;
 import javax.enterprise.inject.Produces;
 import javax.enterprise.inject.spi.InjectionPoint;
 
-import org.apache.commons.configuration.Configuration;
-
-import am.ajf.core.ApplicationContext;
+import am.ajf.core.beans.BeansManager;
 import am.ajf.core.mail.MailSender;
-import am.ajf.core.mail.impl.SimpleMailSenderImpl;
-import am.ajf.core.utils.BeanUtils;
-
-import com.google.common.base.Strings;
 
 public class MailSenderProducer {
 	
+	/*
 	private static final String DEFAULT_MAIL_SESSION_JNDI_NAME = "mail/Session";
 	private static final String MAIL_SENDER_JNDI_NAME_PROPERTY = "mailSender.jndiName";
+	*/
 	
 	public MailSenderProducer() {
 		super();
 	}
 	
 	@Produces
-	public MailSender produceMailSender(InjectionPoint ip) {
+	public MailSender produceMailSender(InjectionPoint ip) throws Exception {
 		
+		/*
 		Configuration config = ApplicationContext.getConfiguration();
 		String jndiName = config.getString(MAIL_SENDER_JNDI_NAME_PROPERTY, DEFAULT_MAIL_SESSION_JNDI_NAME);
 		if (Strings.isNullOrEmpty(jndiName)) {
@@ -35,11 +33,10 @@ public class MailSenderProducer {
 					.concat(MAIL_SENDER_JNDI_NAME_PROPERTY)
 					.concat("' is null or empty."));
 		}
+		*/
 		
-		SimpleMailSenderImpl mailSenderImpl = BeanUtils.newInstance(SimpleMailSenderImpl.class);
-		mailSenderImpl.setJndiName(jndiName);
-		
-		return mailSenderImpl;
+		MailSender mailSender = BeansManager.getDefaultBean(MailSender.class);
+		return mailSender;
 		
 	}
 	
