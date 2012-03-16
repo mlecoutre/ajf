545a15c4a0d4c895503c3672c2e833b8b03fb4ff
u002617 <u002617@39edf04f-2723-2346-8cc1-5ef793c3d051>
2012-03-16 13:05:40 +0000

    disable
    git-svn-id: http://web-svn-srv/repos/ITSWE/ajf/trunk@3731 39edf04f-2723-2346-8cc1-5ef793c3d051

--- ajf-injection/src/main/java/am/ajf/injection/MailSenderProducer.java
+++ ajf-injection/src/main/java/am/ajf/injection/MailSenderProducer.java
@@ -1,12 +1,10 @@
 package am.ajf.injection;
 
 //import javax.enterprise.inject.Produces;
-import javax.enterprise.inject.Produces;
 import javax.enterprise.inject.spi.InjectionPoint;
 
 import am.ajf.core.beans.BeansManager;
 import am.ajf.core.mail.MailSender;
-import am.ajf.injection.annotation.BeanRole;
 
 public class MailSenderProducer {
 
@@ -20,37 +18,27 @@ public class MailSenderProducer {
 		super();
 	}
 
-	@Produces
+	//@Produces
 	public MailSender produceMailSender(InjectionPoint ip) throws Exception {
 
-		/*
-		 * Configuration config = ApplicationContext.getConfiguration(); String
-		 * jndiName = config.getString(MAIL_SENDER_JNDI_NAME_PROPERTY,
-		 * DEFAULT_MAIL_SESSION_JNDI_NAME); if (Strings.isNullOrEmpty(jndiName))
-		 * { String who = ip.getMember().getDeclaringClass().getName()
-		 * .concat("#").concat(ip.getMember().getName());
-		 * 
-		 * throw new RuntimeException("The field injection for '".concat(who)
-		 * .concat("' can not be done because the property '")
-		 * .concat(MAIL_SENDER_JNDI_NAME_PROPERTY)
-		 * .concat("' is null or empty.")); }
-		 */
-
 		/** extract the required Beanrole **/
-		String beanRole = null;
+		//String beanRole = null;
+		/*
 		if (ip.getAnnotated().isAnnotationPresent(BeanRole.class)) {
 			BeanRole beanRoleAnnotation = ip.getAnnotated().getAnnotation(
 					BeanRole.class);
 			beanRole = beanRoleAnnotation.value();
 		}
-
+		*/
 		MailSender mailSender = null;
-		if (null == beanRole) {
+		//if (null == beanRole) {
 			mailSender = BeansManager.getDefaultBean(MailSender.class);
+		/*
 		}
 		else {
 			mailSender = BeansManager.getBean(MailSender.class, beanRole);
 		}
+		*/
 		return mailSender;
 
 	}
